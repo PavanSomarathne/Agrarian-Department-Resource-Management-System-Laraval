9c0f8bd312d4632b4a66b8661a6452de575961b5
ushanfernando <ushansankalpafernando@gmail.com>
2021-08-20 00:08:00 +0530

    added admin dashbaord template

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+   
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="images/favicon.png">
+      <title>Admin Panel</title>
+      <!-- CSS FILES START -->
+      <link href={{asset('assets/css/app.css')}} rel="stylesheet">
+        <link href={{asset('assets/css/bootstrap.min.css')}} rel="stylesheet">
+      <link href={{asset('assets/css/bootstrap-extended.css')}} rel="stylesheet">
+      <link href={{asset('assets/css/bootstrap-min.css.map')}} rel="stylesheet">
+      <link href={{asset('assets/css/dark-theme.css')}} rel="stylesheet">
+      <link href={{asset('assets/css/header-colors.css')}} rel="stylesheet">
+      <link href={{asset('assets/css/icons.css')}} rel="stylesheet">
+      <link href={{asset('assets/css/pace.min.css')}} rel="stylesheet">
+      <link href={{asset('assets/css/semi-dark.css')}} rel="stylesheet">
+      <!-- CSS FILES End -->
+   </head>
+
+   <script src="js/jquery-3.3.1.min.js"></script> 
+      <script src={{asset('assets/js/jquery.min.js')}}></script> 
+      <script src={{asset('assets/js/index.js')}}></script> 
+      <script src={{asset('assets/js/bootstrap.bundle.min.js')}}></script> 
+      <script src={{asset('assets/js/bootstrap.bundle.min.js.map')}}></script> 
+      <script src={{asset('assets/js/pace.min.js')}}></script> 
+      <script src={{asset('assets/js/widgets.js')}}></script> 
+      <script src={{asset('assets/js/app.js')}}></script>
+   
